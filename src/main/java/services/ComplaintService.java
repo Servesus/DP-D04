16ab68ead46b58822f7d0e4bbd38322353636f43
sergioperez1998 <sergio-pm@hotmail.com>
2018-11-23 19:27:46 +0100

    servicios medio hechos

--- src/main/java/services/ComplaintService.java
+++ src/main/java/services/ComplaintService.java
@@ -1,11 +1,15 @@
 
 package services;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import repositories.ComplaintRepository;
+import domain.Complaint;
 
 @Service
 @Transactional
@@ -13,4 +17,41 @@ public class ComplaintService {
 
 	@Autowired
 	private ComplaintRepository	complaintRepository;
+
+
+	public Complaint create() {
+		Complaint result;
+		result = new Complaint();
+		return result;
+	}
+
+	public Collection<Complaint> findAll() {
+		Collection<Complaint> result;
+		Assert.notNull(this.complaintRepository);
+		result = this.complaintRepository.findAll();
+		return result;
+	}
+
+	public Complaint findOne(final int complaintId) {
+		Complaint result;
+		Assert.notNull(this.complaintRepository);
+		result = this.complaintRepository.findOne(complaintId);
+		return result;
+
+	}
+
+	public Complaint save(final Complaint complaint) {
+
+		Complaint result;
+		Assert.notNull(complaint);
+		result = this.complaintRepository.save(complaint);
+		return result;
+	}
+
+	public void delete(final Complaint complaint) {
+		Assert.notNull(complaint);
+		assert complaint.getId() != 0;
+		Assert.isTrue(this.complaintRepository.exists(complaint.getId()));
+		this.complaintRepository.delete(complaint);
+	}
 }
--- src/main/java/services/FixUpTaskService.java
+++ src/main/java/services/FixUpTaskService.java
@@ -1,11 +1,15 @@
 
 package services;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import repositories.FixUpTaskRepository;
+import domain.FixUpTask;
 
 @Service
 @Transactional
@@ -13,5 +17,42 @@ public class FixUpTaskService {
 
 	@Autowired
 	private FixUpTaskRepository	fixUpTaskRepository;
+
+
+	public FixUpTask create() {
+		FixUpTask result;
+		result = new FixUpTask();
+		return result;
+	}
+
+	public Collection<FixUpTask> findAll() {
+		Collection<FixUpTask> result;
+		Assert.notNull(this.fixUpTaskRepository);
+		result = this.fixUpTaskRepository.findAll();
+		return result;
+	}
+
+	public FixUpTask findOne(final int fixUpTaskId) {
+		FixUpTask result;
+		Assert.notNull(this.fixUpTaskRepository);
+		result = this.fixUpTaskRepository.findOne(fixUpTaskId);
+		return result;
+
+	}
+
+	public FixUpTask save(final FixUpTask fixUpTask) {
+
+		FixUpTask result;
+		Assert.notNull(fixUpTask);
+		result = this.fixUpTaskRepository.save(fixUpTask);
+		return result;
+	}
+
+	public void delete(final FixUpTask fixUpTask) {
+		Assert.notNull(fixUpTask);
+		assert fixUpTask.getId() != 0;
+		Assert.isTrue(this.fixUpTaskRepository.exists(fixUpTask.getId()));
+		this.fixUpTaskRepository.delete(fixUpTask);
+	}
 
 }
