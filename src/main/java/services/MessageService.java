59969ee00b650ff7b3ca7b177dd4960e564599ea
Servesus <jesusgarrocho1998@gmail.com>
2018-11-24 21:57:57 +0100

    Mas servicios

---
+++ src/main/java/services/MessageService.java
@@ -0,0 +1,53 @@
+
+package services;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+import repositories.MessageRepository;
+import domain.Message;
+
+@Service
+@Transactional
+public class MessageService {
+
+	//Managed repository
+	private MessageRepository	messageRepository;
+
+
+	//Simple CRUD Methods
+	public List<Message> findAll() {
+		return this.messageRepository.findAll();
+	}
+
+	public Message findOne(final Integer arg0) {
+		return this.messageRepository.findOne(arg0);
+	}
+
+	public Message create() {
+		Message result;
+		result = new Message();
+		//TODO result.setSender = actor logeado
+		return result;
+	}
+
+	public Message save(final Message message) {
+		Assert.notNull(message);
+		final Date currentMoment = new Date();
+		message.setSendDate(currentMoment);
+		final Message result = this.messageRepository.save(message);
+		return result;
+	}
+
+	public void delete(final Message message) {
+		Assert.notNull(message);
+		Assert.isTrue(message.getId() != 0);
+
+		this.messageRepository.delete(message);
+	}
+
+}
