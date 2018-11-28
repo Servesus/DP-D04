f015632e16ddb0016a0cef24f5cef400fb1a96a0
sergioperez1998 <sergio-pm@hotmail.com>
2018-11-28 12:50:52 +0100

    Complaint Service

--- src/test/java/services/ComplaintServiceTest.java
+++ src/test/java/services/ComplaintServiceTest.java
@@ -10,8 +10,6 @@ import org.springframework.util.Assert;
 
 import utilities.AbstractTest;
 import domain.Complaint;
-import domain.Customer;
-import domain.FixUpTask;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
@@ -23,6 +21,7 @@ public class ComplaintServiceTest extends AbstractTest {
 	private ComplaintService	complaintService;
 	private CustomerService		customerService;
 	private FixUpTaskService	fixUpTaskService;
+	private RefereeService		refereeService;
 
 
 	@Test
@@ -35,28 +34,56 @@ public class ComplaintServiceTest extends AbstractTest {
 			&& result.getFixUpTasks().equals(this.complaintService.findOne(result.getId()).getFixUpTasks()));
 
 	}
-	@Test
-	public void testFindOneComplaint() {
-
-		final Customer ejemplo1 = this.customerService.findOne(2622);
-		final FixUpTask ejemplo = this.fixUpTaskService.findOne(2696);
-		final Complaint result = this.complaintService.findOne(2715);
-		Assert.isTrue(result.getTicker() == "103778-j9t643" && result.getDescription() == "esta es la descripcion de complaint1" && result.getCustomer().equals(ejemplo1) && result.getFixUpTasks().equals(ejemplo));
-
-	}
-	@Test
-	public void testFindAllComplaint() {
-
-		Integer comparador = 0;
-		final Complaint[] metodo = (Complaint[]) this.complaintService.findAll().toArray();
-		final FixUpTask[] apoyo = (FixUpTask[]) this.fixUpTaskService.findAll().toArray();
-		for (int i = 0; i < apoyo.length; i++)
-			comparador += apoyo[i].getComplaints().size();
-		Assert.isTrue(metodo.length == comparador);
-
-	}
-	@Test
-	public void testSaveComplaint() {
-
-	}
+	//	@Test
+	//	public void testFindOneComplaint() {
+	//
+	//		final Customer ejemplo1 = this.customerService.findOne(2622);
+	//		final FixUpTask ejemplo = this.fixUpTaskService.findOne(2696);
+	//		final Complaint result = this.complaintService.findOne(2715);
+	//		Assert.isTrue(result.getTicker() == "103778-j9t643" && result.getDescription() == "esta es la descripcion de complaint1" && result.getCustomer().equals(ejemplo1) && result.getFixUpTasks().equals(ejemplo));
+	//
+	//	}
+	//	@Test
+	//	public void testFindAllComplaint() {
+	//
+	//		Integer comparador = 0;
+	//		final Complaint[] metodo = (Complaint[]) this.complaintService.findAll().toArray();
+	//		final FixUpTask[] apoyo = (FixUpTask[]) this.fixUpTaskService.findAll().toArray();
+	//		for (int i = 0; i < apoyo.length; i++)
+	//			comparador += apoyo[i].getComplaints().size();
+	//		Assert.isTrue(metodo.length == comparador);
+	//
+	//	}
+	//	@Test
+	//	public void testSaveComplaint() {
+	//
+	//		super.authenticate("customer1");
+	//		final Complaint creado = this.complaintService.create(2696);
+	//		creado.setDescription("");
+	//		this.complaintService.save(creado);
+	//		final Complaint prueba = this.complaintService.findOne(0);
+	//		Assert.isTrue(creado.equals(prueba));
+	//		super.authenticate(null);
+	//	}
+	//	@Test
+	//	public void testDeleteComplaint() {
+	//		super.authenticate("customer1");
+	//		final Complaint caso1 = this.complaintService.findOne(2715);
+	//		this.complaintService.delete(caso1);
+	//		final Collection<Complaint> all = this.complaintService.findAll();
+	//		Assert.isTrue(!(all.contains(caso1)));
+	//		super.authenticate(null);
+	//	}
+	//	@Test
+	//	public void testSelfAssignedComplaint() {
+	//		super.authenticate("referee1");
+	//		final Collection<Complaint> result = this.complaintService.getComplaintSelfAssigned();
+	//		final Referee nuevo = this.refereeService.findOne(2684);
+	//		final Collection<Complaint> res = new ArrayList<Complaint>();
+	//		final Report[] apoyo = (Report[]) nuevo.getReports().toArray();
+	//		for (int i = 0; i < apoyo.length; i++)
+	//			res.add(apoyo[i].getComplaint());
+	//		Assert.isTrue(result.containsAll(res));
+	//		super.authenticate(null);
+	//	}
 }
