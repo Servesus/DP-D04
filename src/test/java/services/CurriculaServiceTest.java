535f362832d58ec71c33526f67a25869cbb0105e
Servesus <jesusgarrocho1998@gmail.com>
2018-11-29 04:55:54 +0100

    manuel anacardo

--- src/test/java/services/CurriculaServiceTest.java
+++ src/test/java/services/CurriculaServiceTest.java
@@ -24,11 +24,6 @@ public class CurriculaServiceTest extends AbstractTest {
 
 	@Autowired
 	private CurriculaService	curriculaService;
-<<<<<<< HEAD
-	@Autowired
-	private HandyWorkerService	handyWorkerService;
-=======
->>>>>>> jesus
 
 
 	@Test
