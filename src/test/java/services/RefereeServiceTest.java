8a726ad69f0e0750826b25a01ed6d9901406f31b
Servesus <jesusgarrocho1998@gmail.com>
2018-11-29 19:22:53 +0100

    Cambios en test

--- src/test/java/services/RefereeServiceTest.java
+++ src/test/java/services/RefereeServiceTest.java
@@ -1,4 +1,7 @@
+
 package services;
+
+import java.util.Collection;
 
 import javax.transaction.Transactional;
 
@@ -9,65 +12,51 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.Assert;
 
-import domain.Referee;
-
 import security.UserAccount;
 import utilities.AbstractTest;
+import domain.Referee;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
-		"classpath:spring/datasource.xml",
-		"classpath:spring/config/packages.xml"})
+	"classpath:spring/datasource.xml", "classpath:spring/config/packages.xml"
+})
 @Transactional
-public class RefereeServiceTest extends AbstractTest{
-	
-	@Autowired 
-	private RefereeService refereeService;
-	
+public class RefereeServiceTest extends AbstractTest {
+
+	@Autowired
+	private RefereeService	refereeService;
+
+
 	@Test
-	public void createTest(){
+	public void createTest() {
 		super.authenticate("admin1");
-		System.out.println("Entro en el método");
-		
-		Referee r = refereeService.create();
-		System.out.println("Comprobamos que no devuelve null y se crea");
+
+		final Referee r = this.refereeService.create();
 		Assert.notNull(r);
-		System.out.println("Se ha creado un referee");
-		System.out.println(r);
 	}
-	
+
 	@Test
-	public void saveTest(){
+	public void saveTest() {
 		super.authenticate("admin1");
 		UserAccount userAccount;
-		
-		System.out.println("Entro en el método");
-		
-		Referee r = refereeService.create();
-		
-		System.out.println("Se crea un referee con el create del servicio ya comprobado");
-		
+
+		final Referee r = this.refereeService.create();
+
 		userAccount = r.getUserAccount();
 		userAccount.setUsername("referee300");
 		userAccount.setPassword("123456789");
-		
+
 		r.setUserAccount(userAccount);
-		
-		System.out.println("Se crea una userAccount y se le pone un username y una password");
-		
+
 		r.setName("Sergio");
 		r.setSurname("Pérez");
 		r.setEmail("sergio@bananapps.com");
 		r.setAddress("Mi calle");
 		r.setPhoto("http://www.mifoto.com");
 		r.setPhoneNumber("689312351");
-		
-		System.out.println("Le añadimos los atributos al referee");
-		
-		Referee referee = refereeService.save(r);
-		
-		System.out.println(refereeService.findOne(referee.getId()));
-		System.out.println(refereeService.findAll());
-		System.out.println("Comprobamos que se añade");
+
+		final Referee referee = this.refereeService.save(r);
+		final Collection<Referee> referees = this.refereeService.findAll();
+		Assert.isTrue(referees.contains(referee));
 	}
 }
